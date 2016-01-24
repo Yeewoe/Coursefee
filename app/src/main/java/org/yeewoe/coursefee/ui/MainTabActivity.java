cbaf0665f533c0657c5da3f3c0ff3c2f3226c3b7
Yeewoe <476904945@qq.com>
2016-01-24 21:57:12 +0800

    新增jar库
    1.volley
    2.gson

--- app/src/main/java/org/yeewoe/coursefee/ui/MainTabActivity.java
+++ app/src/main/java/org/yeewoe/coursefee/ui/MainTabActivity.java
@@ -1,10 +1,18 @@
 package org.yeewoe.coursefee.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * 主界面
  * Created by wyw on 2016/1/14.
  */
 public class MainTabActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
 }
